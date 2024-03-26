6b5cde892f43fb07e74a9c93ef86280e65cb1692
EMILY_BATES <ebates@bbilogistics.com>
2024-03-26 16:42:16 -0400

    installed libraries - nextui and prisma

---
+++ src/app/providers.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import { NextUIProvider } from "@nextui-org/react"
+
+interface ProvidersProps {
+  children: React.ReactNode
+}
+
+export default function Providers({ children }: ProvidersProps) {
+  return (
+    <NextUIProvider>{children}</NextUIProvider>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,13 @@
 import type { Config } from "tailwindcss";
+import { nextui } from "@nextui-org/react";
 
 const config: Config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+      //add this line for nextui
+    './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
@@ -15,6 +18,8 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  // add 'darkMode: 'class',' and nextui() inside plugins [] for nextui to work
+  darkMode: 'class',
+  plugins: [nextui()],
 };
 export default config;
